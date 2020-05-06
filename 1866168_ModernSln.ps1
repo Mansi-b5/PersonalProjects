f44ee5c05c26a749a27b865912e405feca9ebbc1
Mansi-b5 <60612351+Mansi-b5@users.noreply.github.com>
2020-05-05 22:20:37 -0400

    A3 PowerShell

---
+++ 1866168_ModernSln.ps1
@@ -0,0 +1,40 @@
+﻿[string] $processName = "Notepad";
+$Path = "./*";
+
+KillThatProcess($processName)
+Bamboozle($Path)
+
+Function KillThatProcess([string] $processName)
+{
+    $process = Get-Process $processName
+    $counter = $process.Count
+
+    [string] $input = Read-Host-Prompt "There are $counter processes with the name $processName, proceed ? Y/N"
+    if($input -eq 'Y'-or $input -eq 'y')
+    {
+       Get-Process - $processName | Stop-Process 
+    }
+    else
+    {
+        Write-Host "Do you have CORONA or something ? That was an Invalid input !" 
+    }
+}
+
+Function Bamboozle()
+{
+    
+    $Letter = -join (65..90) + (97..122) | Get-Random -Count 1 | % {[char]$_} 
+
+    Write-Host -BackgroundColor red -ForegroundColor Yellow  " ///////////////////////////// "
+    Write-Host -backgroundColor red -ForegroundColor Yellow  " //YOU HAVE THE CORONA VIRUS// "                                                        
+    Write-Host -backgroundColor red -ForegroundColor Yellow  " ///////////////////////////// " 
+
+    Write-Host "You have chosen the letter $letter"
+
+    [string] $listOfFiles = Get-ChildItem $Path  -Include $letter* |Remove-Item -whatif
+    
+    if($listOfFiles -eq $null)
+    {
+       Write-Host "You have no files with that letter" 
+    }
+}
